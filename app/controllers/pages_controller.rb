f18ab6d8c079742b753bc92fe39dba39ad87dc40
Greg <gsparks@westmont.edu>
2010-04-20 20:35:32 -0700

    commit to git from gsparks

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,25 @@
+class PagesController < ApplicationController
+  def index
+    
+  end
+
+  def home
+
+  end
+
+  def resume
+
+  end
+
+  def cs30
+
+  end
+
+  def cs30
+
+  end
+
+  def cs50
+    
+  end
+end
