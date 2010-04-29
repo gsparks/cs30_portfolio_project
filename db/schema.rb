d8ac0ecfd18070331f5b378d516629ae88e4f1e4
Greg <gsparks@westmont.edu>
2010-04-28 19:22:30 -0700

    --all

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100420231836) do
+ActiveRecord::Schema.define(:version => 20100428231938) do
+
+  create_table "comments", :force => true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.text     "body"
+    t.integer  "post_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "posts", :force => true do |t|
     t.string   "title"
